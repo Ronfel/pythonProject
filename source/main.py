f119a269843496cfab8b0968e60e14fd2fdcd71a
Ronfel <ronfelara@gmail.com>
2023-12-06 23:31:50 -0300

    Criada a Tela principal e alguns menus de cadastro

--- source/main.py
+++ source/main.py
@@ -1,54 +1,40 @@
-import subprocess
-from reportlab.lib.pagesizes import landscape, letter
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
-from reportlab.lib import colors
+import tkinter as tk
+import Clientes
+import Fornecedores
+import Pedidos
+import Produtos
 
-def generate_report(file_path, order_data):
-    pdf = SimpleDocTemplate(file_path, pagesize=landscape(letter))
+class TelaPrincipalApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Tela Principal")
 
-    # Lista para armazenar dados da tabela
-    table_data = []
+        # Barra de menus
+        self.menu_bar = tk.Menu(root)
+        root.config(menu=self.menu_bar)
 
-    # Adiciona itens do pedido
-    table_data.append(["Número do\n Pedido:", "Data do Pedido:", "Cliente:", "Produto", "Quant.", "Preço Unitário", "Subtotal"])
-    for item in order_data['itens']:
-        table_data.append([order_data['numero_pedido'], order_data['data_pedido'], order_data['cliente'], item['produto'], str(item['qtde']), f"R${item['valor_unt']:.2f}", f"R${item['subtotal']:.2f}"])
+        # Menu Cadastros
+        self.menu_cadastros = tk.Menu(self.menu_bar, tearoff=0)
+        self.menu_bar.add_cascade(label="Cadastros", menu=self.menu_cadastros)
+        self.menu_cadastros.add_command(label="Clientes", command=self.abrir_cadastro_clientes)
+        self.menu_cadastros.add_command(label="Produtos", command=self.abrir_cadastro_produtos)
+        self.menu_cadastros.add_command(label="Fornecedores", command=self.abrir_cadastro_fornecedores)
+        self.menu_cadastros.add_separator()
+        self.menu_cadastros.add_command(label="Sair", command=root.destroy)
 
-    # Adiciona informações do recibo
-    total_amount = sum(item['subtotal'] for item in order_data['itens'])
-    table_data.append(["Recibo:", "", "", "", "", "", f"Total: R${total_amount:.2f}"])
+    def abrir_cadastro_clientes(self):
+        root = tk.Toplevel(self.root)
+        app = Clientes.CadastroClientesApp(root)
 
-    # Cria a tabela
-    order_table = Table(table_data, colWidths=[60, 100, 100, 150, 50, 100, 100])
+    def abrir_cadastro_produtos(self):
+        root = tk.Toplevel(self.root)
+        app = Produtos.CadastroProdutosApp(root)
 
-    # Aplica estilos à tabela
-    style = TableStyle([('BACKGROUND', (0, 0), (-1, 0), colors.grey),
-                        ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
-                        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-                        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-                        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
-                        ('BACKGROUND', (0, 1), (-1, 1), colors.beige),
-                        ('GRID', (0, 0), (-1, -1), 1, colors.black)])
+    def abrir_cadastro_fornecedores(self):
+        root = tk.Toplevel(self.root)
+        app =  Fornecedores.CadastroFornecedoresApp(root)
 
-    order_table.setStyle(style)
-
-    # Constrói o PDF com a tabela
-    pdf.build([order_table])
-
-    # Abre o visualizador de PDF padrão do sistema
-    subprocess.Popen(["start", "", file_path], shell=True)
-
-# Dados para o relatório
-dados_pedido = {
-    "numero_pedido": "123456",
-    "data_pedido": "01/12/2023",
-    "cliente": "SpaceX",
-    "itens": [
-        {"produto": "Produto A", "qtde": 2, "valor_unt": 50.00, "subtotal": 100.00},
-        {"produto": "Produto B", "qtde": 1, "valor_unt": 30.00, "subtotal": 30.00},
-        {"produto": "Produto C", "qtde": 3, "valor_unt": 20.00, "subtotal": 60.00}
-    ]
-}
-
-# Estudando geração de relatório
-generate_report("pedido_e_recibo_colunado_com_titulo.pdf", dados_pedido)
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = TelaPrincipalApp(root)
+    root.mainloop()
